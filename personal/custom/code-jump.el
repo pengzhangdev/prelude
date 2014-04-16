189f4ace9c9453093e22781be53508956cf74ff3
Werther <werther0331@gmail.com>
2014-04-16 21:58:22 +0800

    refine code without closure function

--- personal/custom/code-jump.el
+++ personal/custom/code-jump.el
@@ -9,20 +9,40 @@
 
 (defvar cj-jump-sequence
   '(gtags-find-symbol
-    cscope-find-this-symbol))
+    cscope-find-global-definition))
 
-(defun build-code-jump-fun ()
-  "build function with current context info"
+(defun cj-do-code-jump (seq)
+  "do code jump"
   (let ((context (cj-pop-context-del nil)))
-    (defun cj-do-code-jump (seq)
-      "Call code jump functions"
-      (if (null seq)
-          (message "Done code jump")
-        (progn
-          (funcall (car seq))
-          (when (and (equal (current-buffer) (nth 0 context))
-                     (equal (point) (nth 1 context)))
-            (cj-do-code-jump (cdr seq))))))))
+    (if (null seq)
+        (message "Done code jump!")
+      (progn
+        (funcall (car seq))
+        (message "current-buffer %s context %s"
+                 (current-buffer) (nth 0 context))
+        ;;(message "pointer %s context %s"
+        ;;       (point) (nth 1 context))
+        (when (and (equal (current-buffer) (nth 0 context))
+                   (equal (point) (nth 1 context)))
+          ;;(message "failed")
+          (cj-do-code-jump (cdr seq)))))))
+
+
+;;; elisp does not support closure function,
+;;; so context always is nil
+;;; And the value in elisp is dynamic scoping defaultly,
+;;; and it does not support lexical scoping
+;; (defun build-code-jump-fun ()
+;;   "build function with current context info"
+;;   (let ((context (cj-pop-context-del nil)))
+;;     `(lambda (seq)
+;;       (if (null seq)
+;;           (message "Done code jump")
+;;         (progn
+;;           (funcall (car seq))
+;;           (when (and (equal (current-buffer) (nth 0 context))
+;;                      (equal (point) (nth 1 context)))
+;;             (cj-do-code-jump (cdr seq))))))))
 
 (defun cj-push-context ()
   "push buffer and point into stack"
@@ -39,15 +59,18 @@
     (when delete
       (setq cj-point-stack (cdr cj-point-stack))
       (setq cj-buffer-stack (cdr cj-buffer-stack)))
-    (list buffer point)))
+    (if (or (null buffer)
+            (null point))
+        nil
+      (list buffer point))))
 
 (defun cj-restore-context ()
   "restore context before code jump"
   (interactive)
   (let (context)
     (setq context (cj-pop-context-del t))
-    (if (not context)
-        (messge "[code-jump] The tags stack is empty")
+    (if (null context)
+        (message "[code-jump] The tags stack is empty")
       (progn
         (switch-to-buffer (nth 0 context))
         (goto-char (nth 1 context))))))
@@ -56,9 +79,6 @@
   "find symbols"
   (interactive)
   (cj-push-context)
-  (funcall (build-code-jump-fun)
-           cj-jump-sequence))
-
-
+  (cj-do-code-jump cj-jump-sequence))
 
 ;;; code-jump.el ends here
