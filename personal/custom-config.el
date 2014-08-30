0ea802db5897fffed46617d418de123cdd6decb0
Werther <werther0331@gmail.com>
2014-08-30 17:14:51 +0800

    * [prelude] update ggtags key binding

--- personal/custom-config.el
+++ personal/custom-config.el
@@ -147,4 +147,5 @@
 (add-hook 'c-mode-common-hook
           (lambda ()
             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-              (ggtags-mode 1))))
+              (ggtags-mode 1)
+              (define-key ggtags-mode-map "\M-," 'pop-tag-mark))))
