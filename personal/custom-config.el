00fe630a5afadb07f66316866d56d52efcc07afe
wertherzhang <wertherzhang@china-liantong.com>
2015-05-11 15:51:53 +0800

    upgrade prelude to latest and merge personal config

--- personal/custom-config.el
+++ personal/custom-config.el
@@ -3,7 +3,16 @@
 
 (prelude-require-packages '(yasnippet highlight-symbol dropdown-list auto-complete
                                       autopair slime ac-slime ac-c-headers smart-compile
-                                      emamux pomodoro))
+                                      emamux pomodoro htmlize xcscope jedi auto-compile
+                                      ggtags auto-complete-clang multi-eshell))
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1)
+              (define-key ggtags-mode-map "\M-," 'pop-tag-mark))
+            (when (derived-mode-p 'c-mode 'c++-mode 'objc-mode)
+              (setq ac-source (append '(ac-source-clang ac-source-yasnippet) ac-source))
+              )))
 
 (mapc 'load (directory-files
              (expand-file-name "custom" prelude-personal-dir) 't "^[^#].*el$"))
@@ -67,6 +76,10 @@
 
 ;;;;;;cscope
 (require 'xcscope)
+(add-hook 'c-mode-common-hook (function cscope-minor-mode))
+(add-hook 'c++-mode-common-hook (function cscope-minor-mode))
+(add-hook 'dired-mode-hook (function cscope-minor-mode))
+
 
 ;;;; semantic
 ;; (require 'semantic)
@@ -84,22 +97,22 @@
 (global-semantic-decoration-mode)
 
 ;;;;gtags
-(autoload 'gtags-mode "gtags" "" t)
-(gtags-mode 1)
-(setq gtags-auto-update 1)
-(global-set-key [f3] 'gtags-find-tag-from-here)
-(global-set-key [f2] 'gtags-pop-stack)
-(global-set-key (kbd "C-c g f") 'gtags-find-tag)
-(global-set-key (kbd "C-c g r") 'gtags-find-rtag)
-(global-set-key (kbd "C-c g u") 'gtags-pop-stack)
-(global-set-key (kbd "C-c g s") 'gtags-find-symbols)
-(global-set-key (kbd "C-c g g") 'gtags-find-with-grep)
-(global-set-key (kbd "C-c g o") 'gtags-select-tag)
-(add-hook 'gtags-select-mode-hook
-          '(lambda ()
-             (setq hl-line-face 'underline)
-             (hl-line-mode 1)
-             ))
+;;(autoload 'gtags-mode "gtags" "" t)
+;;(gtags-mode 1)
+;;(setq gtags-auto-update 1)
+;;(global-set-key [f3] 'gtags-find-tag-from-here)
+;;(global-set-key [f2] 'gtags-pop-stack)
+;;(global-set-key (kbd "C-c g f") 'gtags-find-tag)
+;;(global-set-key (kbd "C-c g r") 'gtags-find-rtag)
+;;(global-set-key (kbd "C-c g u") 'gtags-pop-stack)
+;;(global-set-key (kbd "C-c g s") 'gtags-find-symbols)
+;;(global-set-key (kbd "C-c g g") 'gtags-find-with-grep)
+;;(global-set-key (kbd "C-c g o") 'gtags-select-tag)
+;;(add-hook 'gtags-select-mode-hook
+;;          '(lambda ()
+;;;             (setq hl-line-face 'underline)
+;;;            (hl-line-mode 1)
+;;             ))
 
 ;; diable withspace and use C-n n instead.
 (setq prelude-whitespace nil)
@@ -128,3 +141,66 @@
 
 ;; emacs tmux
 (require 'emamux)
+
+;; enable highlight when export to html
+;; This require htmlize.el
+(setq org-src-fontify-natively t)
+
+;; disabel 2 window swap
+(define-key prelude-mode-map (kbd "C-c s") nil)
+
+;; python development env
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+(setq jedi:use-shortcuts t) ;; using M-. and M-, to jump code
+
+;;; jedi install guide
+;;; Install Python server (jediepcserver.py) by running
+;;;
+;;; M-x jedi:install-server in Emacs
+;;; (see also jedi:install-server).
+;;;
+;;;out of emacs, install python-virtualenv.
+;;;apt-get install python-virtualenv
+;; eshell prompt
+
+(defmacro with-face (str &rest properties)
+    `(propertize ,str 'face (list ,@properties)))
+
+(defun shortened-path (path max-len)
+  "Return a modified version of `path', replacing some components
+      with single characters starting from the left to try and get
+      the path down to `max-len'"
+  (let* ((components (split-string (abbreviate-file-name path) "/"))
+         (len (+ (1- (length components))
+                 (reduce '+ components :key 'length)))
+         (str ""))
+    (while (and (> len max-len)
+                (cdr components))
+      (setq str (concat str (if (= 0 (length (car components)))
+                                "/"
+                              (string (elt (car components) 0) ?/)))
+            len (- len (1- (length (car components))))
+            components (cdr components)))
+    (concat str (reduce (lambda (a b) (concat a "/" b)) components))))
+
+(defun shk-eshell-prompt ()
+  (let ((header-bg "#fff"))
+    (concat
+     (with-face user-login-name :foreground "blue")
+     "@"
+     (with-face "localhost" :foreground "green")
+     (with-face (concat ":" (shortened-path  (eshell/pwd) 40)) :foreground "#689")
+     ;; (with-face (format-time-string "(%Y-%m-%d %H:%M) " (current-time)) :background header-bg :foreground "#888") 
+     (with-face
+      (or (ignore-errors (format "(%s)" (vc-responsible-backend default-directory))) "")
+      :foreground header-bg)
+     ;; (with-face "\n" :background header-bg) 
+     (if (= (user-uid) 0)
+         (with-face " #" :foreground "red")
+       " $")
+     " ")))
+(setq eshell-prompt-function 'shk-eshell-prompt)
+(setq eshell-highlight-prompt nil)
+
+(define-key input-decode-map "\e[1;2A" [S-up])
